d9686caee1f8e91e37508f6b1c900e30b4cad028
Pretty-Horizontal <michelle@prettyhorizontal.com>
2016-02-06 15:21:18 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    include 'includes/header.php';
+?>
+
+    <div class="container page_wrapper">
+
+      <div class="starter_template">
+            <h1>michelle abbasipour</h1>
+        
+        <!-- contact details --> 
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">w: michelleabbasipour.com</div>
+            <div class="col-xs-6 col-sm-4">t: 07568 530324</div>
+            <div class="col-xs-6 col-sm-4">e: michelle2.abbasipour@live.uwe.ac.uk</div>
+        </div>
+        
+        <!-- main content -->
+        <div class="row">
+            
+            <!-- sidebar -->
+            <div class="col-md-4">sidebar
+            
+            
+            </div>
+            
+            <!-- main details -->
+            <div class="col-md-8">large column
+            
+            </div>
+        </div>
+          
+      </div>
+
+    </div><!-- /.container -->
+
+<?php
+    include 'includes/footer.php';
+?>
